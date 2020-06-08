d08f566075e3713d1b0372444dca26c5bdf14c5f
kenny <kennyooi@inwebson.com>
2020-06-08 15:23:27 +0800

    Updates

---
+++ precache-manifest.f36f5302cb2318016b4488fd1f38d926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388fe443856acbc0761a8797e29dff5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa0a2607e5d8c4a2094c",
+    "url": "./static/css/2.1d5ea050.chunk.css"
+  },
+  {
+    "revision": "44f0c6095c15a6599da8",
+    "url": "./static/css/main.059e17c7.chunk.css"
+  },
+  {
+    "revision": "fa0a2607e5d8c4a2094c",
+    "url": "./static/js/2.49b7b25d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.49b7b25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f0c6095c15a6599da8",
+    "url": "./static/js/main.856a142e.chunk.js"
+  },
+  {
+    "revision": "e6e3cffe71f07cd4170f",
+    "url": "./static/js/runtime-main.0381be4a.js"
+  }
+]);
